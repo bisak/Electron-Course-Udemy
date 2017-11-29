459c232b54ee9119037f3813bd13b5951469d6ce
biskazz <biser.p.atanasov@gmail.com>
2017-11-29 11:46:46 +0200

    Course completed

--- Converter-App/index.js
+++ Converter-App/index.js
@@ -1,6 +1,8 @@
 const electron = require('electron')
+const ffmpeg = require('fluent-ffmpeg')
+const _ = require('lodash')
 
-const { app, BrowserWindow, ipcMain } = electron
+const { app, BrowserWindow, ipcMain, shell } = electron
 
 let mainWindow
 app.on('ready', () => {
@@ -13,6 +15,40 @@ app.on('ready', () => {
 })
 
 ipcMain.on('videos:added', (event, videos) => {
-  console.log(videos);
-  
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        if (err) return reject(err)
+        video.duration = metadata.format.duration
+        video.format = 'avi'
+        return resolve(video)
+      })
+    })
+  })
+
+  Promise.all(promises).then((results) => {
+    mainWindow.webContents.send('metadata:complete', results)
+  })
+})
+
+ipcMain.on('conversion:start', (event, videos) => {
+  _.each(videos, video => {
+    const outputDirectory = video.path.split(video.name)[0]
+    const outputName = video.name.split('.')[0]
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`
+
+    ffmpeg(video.path)
+      .output(outputPath)
+      .on('end', () => {
+        mainWindow.webContents.send('conversion:end', { video, outputPath })
+      })
+      .on('progress', ({ timemark }) => {
+        mainWindow.webContents.send('conversion:progress', { video, timemark })
+      })
+      .run()
+  })
+})
+
+ipcMain.on('folder:open', (event, outputPath) => {
+  shell.showItemInFolder(outputPath)
 })
